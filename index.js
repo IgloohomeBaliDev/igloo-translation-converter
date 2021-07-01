6ce3e7e57df2d001746bcaf38169a6e85a869080
Aditya Wikardiyan <aditya@igloohome.co>
2021-07-01 21:31:20 +0800

    Add readme.md

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-/** DOCUMENTATION
- * 
- * ----
- * Requirement
- * - Node.js
- * - NPM
- * 
- * ----
- * Run
- * 
- * [find-not-translate]
- *  1. add file en.json for the main file translation inside `input/find-not-translate` directory
- *  2. add the translation file on `input/find-not-translate` directory, for example: ja.json
- *  3. run 'node index.js {type} {file_name}' on terminal/cmd, example: 'node index.js find-not-translate ja.json' 
- *    - type, find-not-translate : [output] result output is translation file from .json (apps) that not translate yet
- *    - file_name, file comparison name
- *  4. the result will be on `output/result-{type}` directory
- * 
- * [csv-to-json]
- *  1. add translated .csv file & translation source .json inside `input/csv-to-json` directory
- *  2. run 'node index.js {type} {file_name} {source_file}' on terminal/cmd, example: 'node index.js csv-to-json translated.csv ja.json' 
- *    - type, csv-to-json : [output] file that already translate by translator to .json (apps)
- *    - file_name, translated file name
- *    - source_file, translation source file .json
- *  3. the result will be on `output/result-{type}` directory
- */
+// Read the readme.md for Documentation
 
 const fs = require('fs');
 
